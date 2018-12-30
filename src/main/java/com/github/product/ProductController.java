99a8c2304422994452d93430f06a610ad48b8db6
Mateusz <spedrasss@gmail.com>
2018-12-30 19:30:11 +0100

    empty controller product

---
+++ src/main/java/com/github/product/ProductController.java
@@ -0,0 +1,11 @@
+package com.github.product;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+
+
+}
