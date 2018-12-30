1a3121d18842f85ddfb9d29a7116b2d6369d849d
Mateusz <spedrasss@gmail.com>
2018-12-30 16:34:58 +0100

    Add homepage controller

---
+++ src/main/java/com/github/homepage/HomePageController.java
@@ -0,0 +1,13 @@
+package com.github.homepage;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomePageController {
+
+    @RequestMapping("/")
+    public String redirectToHomePage(){
+        return "homepage";
+    }
+}
