cf57f0b5dae06fbb06b3da1e17a021997b971e25
Mateusz <spedrasss@gmail.com>
2018-12-30 19:59:40 +0100

    Add simple version entity Product

--- src/main/java/com/github/product/Product.java
+++ src/main/java/com/github/product/Product.java
@@ -1,4 +1,33 @@
 package com.github.product;
 
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(min = 3, max = 32)
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
