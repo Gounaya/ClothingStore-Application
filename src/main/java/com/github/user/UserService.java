511256f53d8578ce6943798496970b2623aca37b
Mateusz <spedrasss@gmail.com>
2018-12-30 17:40:33 +0100

    Add user service

--- src/main/java/com/github/user/UserService.java
+++ src/main/java/com/github/user/UserService.java
@@ -1,4 +1,15 @@
 package com.github.user;
 
+import java.util.List;
+
 public interface UserService {
+    void save(User user);
+
+    void update(User user);
+
+    User find(Long id);
+
+    void delete(Long id);
+
+    List<User> findAll();
 }
