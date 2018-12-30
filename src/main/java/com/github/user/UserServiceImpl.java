1ea36208c2bd01057b6a7cfa6ba7c2d23a82af42
Mateusz <spedrasss@gmail.com>
2018-12-30 19:51:15 +0100

    Implement method userservice interface

--- src/main/java/com/github/user/UserServiceImpl.java
+++ src/main/java/com/github/user/UserServiceImpl.java
@@ -1,4 +1,36 @@
 package com.github.user;
 
-public class UserServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public void update(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public User find(Long id) {
+        return userRepository.findOne(id);
+    }
+
+    @Override
+    public void delete(Long id) {
+        userRepository.delete(id);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
 }
